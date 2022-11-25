72a8c63592b72543ec6e83550336043a1ebc9e47
naka <nakasyo2792@gmail.com>
2022-11-25 22:40:13 +0900

    add opencv #1

--- monitercamera/urls.py
+++ monitercamera/urls.py
@@ -1,3 +1,6 @@
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+
 from django.urls import path
 from . import views
 
@@ -6,5 +9,7 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
     path('inquiry/', views.InquiryView.as_view(), name="inquiry"),
     path('camera/', views.CameraView.as_view(), name="camera"),
+    path('video_feed', views.video_feed_view(), name="video_feed"),
     path('camera_photo/', views.CameraPhotoView.as_view(), name="camera_photo"),
 ]
+urlpatterns += staticfiles_urlpatterns()
--- monitercamera/views.py
+++ monitercamera/views.py
@@ -9,8 +9,7 @@ from django.shortcuts import get_object_or_404
 
 
 logger = logging.getLogger(__name__)
-class IndexView(generic.TemplateView):
-    template_name = "index.html"
+
 
 class InquiryView(generic.FormView):
     template_name = "inquiry.html"
@@ -28,3 +27,42 @@ class CameraView(LoginRequiredMixin, generic.TemplateView):
 
 class CameraPhotoView(LoginRequiredMixin, generic.TemplateView):
     template_name = 'camera_photo.html'
+
+
+
+import cv2
+from django.http import StreamingHttpResponse
+from django.shortcuts import render
+from django.views import View
+
+
+# ストリーミング画像・映像を表示するview
+class IndexView(generic.TemplateView):
+
+    def get(self, request):
+        return render(request, 'index.html', {})
+
+# ストリーミング画像を定期的に返却するview
+def video_feed_view():
+    return lambda _: StreamingHttpResponse(generate_frame(), content_type='multipart/x-mixed-replace; boundary=frame')
+
+# フレーム生成・返却する処理
+def generate_frame():
+    capture = cv2.VideoCapture(0)  # USBカメラから
+
+    while True:
+        if not capture.isOpened():
+            print("Capture is not opened.")
+            break
+        # カメラからフレーム画像を取得
+        ret, frame = capture.read()
+        if not ret:
+            print("Failed to read frame.")
+            break
+        # フレーム画像バイナリに変換
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        byte_frame = jpeg.tobytes()
+        # フレーム画像のバイナリデータをユーザーに送付する
+        yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + byte_frame + b'\r\n\r\n')
+    capture.release()
